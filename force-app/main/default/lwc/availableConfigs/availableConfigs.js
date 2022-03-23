1e32c76f6afba6f92ba246018f344821a302b720
shruti.grg <shrutigarg193@gmail.com>
2022-03-23 23:25:27 +0530

    ClassChanges

--- force-app/main/default/lwc/availableConfigs/availableConfigs.js
+++ force-app/main/default/lwc/availableConfigs/availableConfigs.js
@@ -1,19 +1,24 @@
-import { LightningElement  , track,wire, api} from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import getAvailableConfig from '@salesforce/apex/CaseConfigController.getAvailableConfig';
 import addCaseConfig from '@salesforce/apex/CaseConfigController.addCaseConfig';
 import getCaseDetails from '@salesforce/apex/CaseConfigController.getCaseDetails';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { getRecordNotifyChange } from 'lightning/uiRecordApi';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import STATUS_FIELD from '@salesforce/schema/Case.Status';
 import CASE_OBJECT from '@salesforce/schema/Case';
-import { publish, MessageContext,subscribe,
+import {
+    publish, MessageContext, subscribe,
     unsubscribe,
-    APPLICATION_SCOPE } from 'lightning/messageService';
+    APPLICATION_SCOPE
+} from 'lightning/messageService';
 
-    import CONFIG_SAVED from '@salesforce/messageChannel/Saved_Config__c';
+import CONFIG_SAVED from '@salesforce/messageChannel/Saved_Config__c';
+const fields = [STATUS_FIELD];
 
 export default class AvailableConfigs extends LightningElement {
-   
+
     subscription;
     @api recordId;
     @track configList = [];
@@ -25,169 +30,153 @@ export default class AvailableConfigs extends LightningElement {
     @track endingRecord = 0;
     @track pageLength = 2;
     @track page = 1;
-    @track status;
+    status;
     @wire(MessageContext)
     messageContext;
     addEvent = false;
 
 
+
     subscribeToMessageChannel() {
 
         this.subscription = subscribe(
-        this.messageContext,
-        CONFIG_SAVED,
-        (message) => this.getConfig()
-        
+            this.messageContext,
+            CONFIG_SAVED,
+            (message) => this.getConfig()
+
         );
-        
-    } 
+
+    }
 
     unsubscribeToMessageChannel() {
         unsubscribe(this.subscription);
         this.subscription = null;
     }
 
-    connectedCallback(){
+    connectedCallback() {
         this.subscribeToMessageChannel();
         this.getConfig(); //load the data
     }
 
+    showToast(title, message, variant) {
+        const toastEvent = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(toastEvent);
+    }
 
-    getConfig(){
+    /* @wire(getRecord, { recordId: '$recordId', fields })
+     case;*/
+
+
+
+
+    getConfig() {
         console.log(this.recordId);
         getAvailableConfig()
-        .then(data => {
-            this.configList=JSON.parse(data);
-            this.totalRecountCount = this.configList.length;
-            this.totalPage = Math.ceil(this.totalRecountCount / this.pageLength);
-            this.data = this.configList.slice(0,this.pageLength); 
-            this.endingRecord = this.pageLength;
-        
-        })
-        .catch(error => {
-            console.log('error---'+JSON.stringify(error));
-            console.log('no case closed');
-            this.showToast('Error',error, 'error');
-        });
+            .then(data => {
+                this.configList = JSON.parse(data);
+                this.totalRecountCount = this.configList.length;
+                this.totalPage = Math.ceil(this.totalRecountCount / this.pageLength);
+                this.data = this.configList.slice(0, this.pageLength);
+                this.endingRecord = this.pageLength;
+
+            })
+            .catch(error => {
+                this.error = error;
+                console.log('no case closed');
+                this.showToast('Error', error, 'error');
+            });
     }
 
-  
-  
-  @wire(getCaseDetails, {caseId: '$recordId'}) getCaseStatus({data,error}){
-      if(data){
-        this.status = data;
-      }
-      else if(error){
-        if (Array.isArray(error.body)) {
-            this.error = error.body.map(e => e.message).join(', ');
-            console.log('error---'+JSON.stringify(this.error));
-        } else if (typeof error.body.message === 'string') {
-            this.error = error.body.message;
-            console.log('error---'+this.error);
-        }
-      }
-  };
-  
-   
-    handleSelected(event){       
-        this.configList[event.target.value].isSelected= event.target.checked;
+    refresh() {
+        console.log('inside refresh' + this.caseStatus.data);
+        //return refreshApex(this.caseStatus); 
     }
-    allSelected(event){
+
+    handleSelected(event) {
+        this.configList[event.target.value].isSelected = event.target.checked;
+    }
+    allSelected(event) {
         for (var i = 0; i < this.configList.length; i++) {
             this.configList[i].isSelected = event.target.checked;
-          }
-    }
-    addSelected(event){
-       this.caseStatus(this.recordId);
-       
-        if(this.status != 'Closed'){
-        for(var i=0 ; i<this.configList.length ; i++){
-           if(this.configList[i].isSelected){
-               this.selectedConfig.push(this.configList[i]);
-               this.configList[i].isSelected = false;
-           }
         }
-        console.log("###Selected Accounts" + this.selectedConfig.length);
-        console.log("###Stringify : " + JSON.stringify(this.selectedConfig));
-        
-        this.handleSelectedConfigs(this.selectedConfig);
     }
-    else{
-        this.showToast('Case Closed','Case Configs cannot be added after Case is closed', 'info');
-       // return refreshApex(this.getConfig());
-    }
-      
+    addSelected(event) {
+
+        getCaseDetails({ caseId: this.recordId })
+            .then(result => {
+                if (result != 'Closed') {
+                    for (var i = 0; i < this.configList.length; i++) {
+                        if (this.configList[i].isSelected) {
+                            this.selectedConfig.push(this.configList[i]);
+                            this.configList[i].isSelected = false;
+                        }
+                    }
+
+                    if (this.selectedConfig.length != 0) {
+                        this.handleSelectedConfigs(this.selectedConfig);
+                    }
+                    else {
+                        this.showToast('Info', 'Please select atleast one record to add!', 'info');
+                    }
+
+                }
+                else {
+                    this.showToast('Case Closed', 'Case Configs cannot be added after Case is closed', 'info');
+                }
+            })
     }
 
-    handleSelectedConfigs(selectedConfig){
-      
-        addCaseConfig({selectedConfig : JSON.stringify(selectedConfig),
-                      caseId : this.recordId})
-        .then(result => {
-            this.addEvent = true;
-                getRecordNotifyChange([{recordId: this.recordId}])
-                publish(this.messageContext,CONFIG_SAVED,null);
-                return refreshApex(this.getConfig());
-           
+    handleSelectedConfigs(selectedConfig) {
+
+        addCaseConfig({
+            selectedConfig: JSON.stringify(selectedConfig),
+            caseId: this.recordId
         })
-        .catch(error => {
-            this.showToast('Error',error, 'error');
-        });
+            .then(result => {
+                this.addEvent = true;
+                publish(this.messageContext, CONFIG_SAVED, null);
+                return refreshApex(this.getConfig());
+            })
+            .catch(error => {
+                this.error = error;
+                console.log('error--' + error);
+                this.showToast('Error', error, 'error');
+            });
     }
-    nextpage(){
-        if((this.page < this.totalPage)){      // && this.page != this.totalPage
+    nextpage() {
+        if ((this.page < this.totalPage)) {      // && this.page != this.totalPage
             this.page = this.page + 1; //increase page by 1
-            this.displayRecordPerPage(this.page);    
-        }  
-    }   
-    prevpage(){
+            this.displayRecordPerPage(this.page);
+        }
+    }
+    prevpage() {
         if (this.page > 1) {
             this.page = this.page - 1; //decrease page by 1
             this.displayRecordPerPage(this.page);
         }
     }
-  
-    displayRecordPerPage(page){
 
-        /*let's say for 2nd page, it will be => "Displaying 6 to 10 of 23 records. Page 2 of 5"
-        page = 2; pageSize = 5; startingRecord = 5, endingRecord = 10
-        so, slice(5,10) will give 5th to 9th records.
-        */
-        this.startingRecord = ((page -1) * this.pageLength) ;
+    displayRecordPerPage(page) {
+
+        this.startingRecord = ((page - 1) * this.pageLength);
         this.endingRecord = (this.pageLength * page);
 
-        this.endingRecord = (this.endingRecord > this.totalRecountCount) 
-                            ? this.totalRecountCount : this.endingRecord; 
+        this.endingRecord = (this.endingRecord > this.totalRecountCount)
+            ? this.totalRecountCount : this.endingRecord;
 
         this.data = this.configList.slice(this.startingRecord, this.endingRecord);
 
         //increment by 1 to display the startingRecord count, 
         //so for 2nd page, it will show "Displaying 6 to 10 of 23 records. Page 2 of 5"
         this.startingRecord = this.startingRecord + 1;
-    }    
-    async caseStatus(recordId){
-        console.log(this.recordId);
-        await getCaseDetails({caseId : this.recordId})
-        .then(result => {           
-                if(result == 'Closed'){
-                    this.status = 'Closed';
-                }          
-        })
-        .catch(error => {
-            
-            this.showToast('Error',error, 'error');
-        });
-        getRecordNotifyChange([{ recordId: this.recordId }]);
     }
 
-    showToast(title,message,variant){
-        const toastEvent = new ShowToastEvent({
-            title: title,
-            message : message,
-            variant:variant
-        });
-        this.dispatchEvent(toastEvent);
-    }
 
-   
+
+
+
 }
--- force-app/main/default/lwc/caseConfigs/caseConfigs.js
+++ force-app/main/default/lwc/caseConfigs/caseConfigs.js
@@ -1,10 +1,11 @@
-import { LightningElement , track , wire ,api } from 'lwc';
-import {refreshApex} from '@salesforce/apex';
+import { LightningElement, track, wire, api } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 import getCaseConfig from '@salesforce/apex/CaseConfigController.getCaseConfig';
 import sendCase from '@salesforce/apex/CaseConfigCalloutService.sendCase';
-
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { getRecordNotifyChange } from 'lightning/uiRecordApi';
 import CONFIG_SAVED from '@salesforce/messageChannel/Saved_Config__c';
+import getCaseDetails from '@salesforce/apex/CaseConfigController.getCaseDetails';
 import {
     publish,
     subscribe,
@@ -16,135 +17,140 @@ import {
 
 
 export default class CaseConfigs extends LightningElement {
-    
+
     @api recordId;
     subscription;
     @track configList = [];
     @track data = [];
-     error;
+    error;
     @track selectedConfig = [];
     totalRecountCount = 0;
-     totalPage = 1;
-     endingRecord = 0;
-     pageLength = 2;
-     page = 1;
+    totalPage = 1;
+    endingRecord = 0;
+    pageLength = 2;
+    page = 1;
     disabled = false;
     toggleVariant = 'brand';
     cssClass = 'slds-button slds-button_neutral';
     @wire(MessageContext)
-    messageContext; 
-   
-   
+    messageContext;
+    status;
+
+
 
     subscribeToMessageChannel() {
 
         this.subscription = subscribe(
-        this.messageContext,
-        CONFIG_SAVED,
-        (message) => this.refreshCaseConfigs(true)
-        
+            this.messageContext,
+            CONFIG_SAVED,
+            (message) => this.refreshCaseConfigs(true)
+
         );
-        
-    } 
+    }
 
     unsubscribeToMessageChannel() {
         unsubscribe(this.subscription);
         this.subscription = null;
     }
-    connectedCallback(){
+
+    connectedCallback() {
         this.subscribeToMessageChannel();
         this.refreshCaseConfigs(false);
-        
+
     }
 
-  
-
-    refreshCaseConfigs(addEvent){
-        getCaseConfig({recordId : this.recordId})
-        .then(data => {
-            
-                console.log('addEvent---'+addEvent);
-                this.configList=JSON.parse(data);
-                console.log('data---'+ this.configList);
-                this.totalRecountCount = this.configList.length;
-                console.log(' this.totalRecountCount---'+ this.totalRecountCount);
-                this.totalPage = Math.ceil(this.totalRecountCount / this.pageLength);
-                this.data = this.configList.slice(0,this.pageLength); 
-                this.endingRecord = this.pageLength;
-               if(addEvent){
-                     refreshCaseConfigs(false);
-                }
-               
-
-        })
-        .catch(error => {
-            this.showToast('Error',error, 'error');
+    showToast(title, message, variant) {
+        const toastEvent = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
         });
-    } 
+        this.dispatchEvent(toastEvent);
+    }
 
-  
+    async refreshCase() {
+        await getCaseDetails({ caseId: '$recordId' })
+            .then(result => {
+                this.status = result;
+            })
+    }
+
+    refreshCaseConfigs(addEvent) {
+        getCaseConfig({ recordId: this.recordId })
+            .then(result => {
+
+                this.configList = JSON.parse(result);
+                this.totalRecountCount = this.configList.length;
+                this.totalPage = Math.ceil(this.totalRecountCount / this.pageLength);
+                this.data = this.configList.slice(0, this.pageLength);
+                this.endingRecord = this.pageLength;
+                if (addEvent) {
+                    this.refreshCaseConfigs(false);
+                }
+
+
+            })
+            .catch(error => {
+                this.error = error;
+                console.log('error  config---' + error);
+                this.showToast('Error', error, 'error');
+            });
+    }
+
+
 
     disconnectedCallback() {
         this.unsubscribeToMessageChannel();
     }
 
-    nextpage(){
-        if((this.page < this.totalPage)){      // && this.page != this.totalPage
+    nextpage() {
+        if ((this.page < this.totalPage)) {      // && this.page != this.totalPage
             this.page = this.page + 1; //increase page by 1
-            console.log(' this.page next page---'+ this.page);
-            this.displayRecordPerPage(this.page);    
-        }  
-    }   
-    prevpage(){
-        if (this.page > 1) {
-            this.page = this.page - 1; //decrease page by 1
-            console.log(' this.page pre page---'+ this.page);
             this.displayRecordPerPage(this.page);
         }
     }
-  
-    displayRecordPerPage(page){
 
-        /*let's say for 2nd page, it will be => "Displaying 6 to 10 of 23 records. Page 2 of 5"
-        page = 2; pageSize = 5; startingRecord = 5, endingRecord = 10
-        so, slice(5,10) will give 5th to 9th records.
-        */
-        this.startingRecord = ((page -1) * this.pageLength) ;
+    prevpage() {
+        if (this.page > 1) {
+            this.page = this.page - 1; //decrease page by 1
+            this.displayRecordPerPage(this.page);
+        }
+    }
+
+    displayRecordPerPage(page) {
+
+        this.startingRecord = ((page - 1) * this.pageLength);
         this.endingRecord = (this.pageLength * page);
 
-        this.endingRecord = (this.endingRecord > this.totalRecountCount) 
-                            ? this.totalRecountCount : this.endingRecord; 
+        this.endingRecord = (this.endingRecord > this.totalRecountCount)
+            ? this.totalRecountCount : this.endingRecord;
 
         this.data = this.configList.slice(this.startingRecord, this.endingRecord);
 
         //increment by 1 to display the startingRecord count, 
         //so for 2nd page, it will show "Displaying 6 to 10 of 23 records. Page 2 of 5"
         this.startingRecord = this.startingRecord + 1;
-    }   
-    sendCaseConfigs(event){
-        sendCase({recordId : this.recordId})
-        .then(result => {
-                if(result == 'Closed'){
-                    this.disabled = true;
-                    this.toggleVariant = destructive;
-                    this.cssClass = 'slds-button slds-button_outline-brand';
-                    getRecordNotifyChange([{recordId: this.recordId}])
-                    publish(this.messageContext,CONFIG_SAVED,null);
-                    this.showToast('Case Status','Case is closed!', 'Success');
-                }
-           
-        })
-        .catch(error => {
-            this.showToast('Error',error, 'error');
-        });
-    } 
+    }
 
-    showToast(title,message,variant){
-        const toastEvent = new ShowToastEvent({
-            title: title,
-            message : message,
-            variant:variant
-        });
-        this.dispatchEvent(toastEvent);
+    sendCaseConfigs(event) {
+        this.disabled = true;
+        sendCase({ recordId: this.recordId })
+            .then(result => {
+                getCaseDetails({ caseId: this.recordId })
+                    .then(result => {
+                        if (result == 'Closed') {
+                            this.status = result;
+                            this.showToast('Case Status', 'Case is closed!', 'Success');
+                            this.cssClass = 'slds-button slds-button_outline-brand';
+                            // getRecordNotifyChange([{recordId: this.recordId}])
+                            publish(this.messageContext, CONFIG_SAVED, null);
+
+                        }
+                    })
+            })
+            .catch(error => {
+                this.error = error;
+                this.showToast('Error', error, 'error');
+            });
     }
 }
